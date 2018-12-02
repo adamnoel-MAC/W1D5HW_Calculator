e2a58c07ea2b3a7aa9d7d7bce396781ced4a6aee
Adam Noel <adam.noel@mobileappscompany.us>
2018-12-02 12:34:37 -0500

    Completed Portrait layout and moved most functionality to Calculator class TODO: Finish -, x, / functions TODO: Finish Landscape layout

--- app/src/main/java/com/mobileapps/w1d5hw_calculator/Calculator.java
+++ app/src/main/java/com/mobileapps/w1d5hw_calculator/Calculator.java
@@ -1,23 +1,118 @@
 package com.mobileapps.w1d5hw_calculator;
 
-import static com.mobileapps.w1d5hw_calculator.MainActivity.ADD_OP;
-import static com.mobileapps.w1d5hw_calculator.MainActivity.NO_OP;
-import static com.mobileapps.w1d5hw_calculator.MainActivity.SUB_OP;
-import static com.mobileapps.w1d5hw_calculator.MainActivity.MUL_OP;
-import static com.mobileapps.w1d5hw_calculator.MainActivity.DIV_OP;
+import android.content.Context;
+import android.util.Log;
 
 public class Calculator {
 
-//    private static final int NO_OP = 0;
-//    private static final int ADD_OP = 1;
-//    private static final int SUB_OP = 2;
-//    private static final int MUL_OP = 3;
-//    private static final int DIV_OP = 4;
+    public static final String TAG = "CALCULATOR";
 
-    int currOp = 0;
-    double Entry = 0.0;
-    int DecPos = 0;
-    double Result = 0.0; //Holds current result
+    private static final int NO_OP = 0;
+    private static final int ADD_OP = 1;
+    private static final int SUB_OP = 2;
+    private static final int MUL_OP = 3;
+    private static final int DIV_OP = 4;
+
+
+    private int currOp = NO_OP;
+    private double Entry = 0.0;
+    private int DecPos = 0;
+    private int zeroes = 0;
+    private double Result = 0.0; //Holds current result
+    private String key = "";
+    private String lastKey = "";
+    private int keyValue = 0;
+
+    private int tmpCurrOp;
+    private double tmpEntry;
+    private int tmpDecPos;
+    private double tmpResult;
+
+    public Calculator() {
+    }
+
+    public void btnClick (String key) {
+        Log.d(TAG, "btnClick: key: " + key);
+
+        // Get current state of things
+        tmpEntry = this.Entry;
+        tmpResult = this.Result;
+        tmpCurrOp = this.currOp;
+        tmpDecPos = this.DecPos;
+
+        switch (key) {
+            case "C":
+                Result = 0.0;
+                currOp = 0;
+            case "CE":
+                Entry = 0.0;
+                DecPos = 0;
+                zeroes = 0;
+                break;
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                if (tmpDecPos > 0) {
+                    Entry = tmpEntry + (Double.parseDouble(key) * Math.pow(10, -tmpDecPos));
+                    zeroes = 0;
+                    DecPos++;
+                    Log.d(TAG, "btnClick: Entry: " + Entry + "; DecPos: " + DecPos);
+                } else {
+                    Entry = (tmpEntry * 10.0) + Double.parseDouble(key);
+                }
+                Log.d(TAG, "btnClick (key: 1-9): Entry: " + Entry + "; DecPos: " + DecPos);
+                break;
+            case "0":
+                if (tmpDecPos > 0) {
+                    zeroes++;
+                    DecPos++;
+                    Log.d(TAG, "btnClick: Entry: " + Entry + "; DecPos: " + DecPos);
+                } else {
+                    Entry = (tmpEntry * 10.0);
+                }
+                Log.d(TAG, "btnClick (key=0): Entry: " + Entry + "; DecPos: " + DecPos);
+                break;
+            case ".":
+                if (tmpDecPos == 0) DecPos++;
+                break;
+            case "+":
+                currOp = ADD_OP;
+                if (Entry > 0.0){
+                    if (Result > 0.0) execute();
+                    else {
+                        setResult(getEntry());
+                        setEntry(0.0);
+                    }
+                }
+                break;
+            case "-":
+                if (currOp > 0) execute();
+                else currOp = SUB_OP;
+                break;
+            case "X":
+                if (currOp > 0) execute();
+                else currOp = MUL_OP;
+                break;
+            case "/":
+                if (currOp > 0) execute();
+                else currOp = DIV_OP;
+                break;
+            case "=":
+                if (currOp > 0) execute();
+                currOp = 0;
+                setEntry(getResult());
+                break;
+            case "+/-":
+                setEntry(Entry * -1.0);
+                break;
+        }
+    }
 
     public void execute(){
         switch (this.currOp){
@@ -67,5 +162,13 @@ public class Calculator {
 
     public void setResult(double result) {
         Result = result;
+    }
+
+    public int getZeroes() {
+        return zeroes;
+    }
+
+    public void setZeroes(int zeroes) {
+        this.zeroes = zeroes;
     }
 }
--- app/src/main/java/com/mobileapps/w1d5hw_calculator/MainActivity.java
+++ app/src/main/java/com/mobileapps/w1d5hw_calculator/MainActivity.java
@@ -6,26 +6,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.security.Key;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MAIN ACTIVITY";
+
     public TextView tvHead;
     public TextView tvDisp;
-    private double tmpEntry;
-    private int tmpDecPos;
-    private double tmpResult;
     private Button btn;
     private String key;
-    private int currOp;
-
-    public static final int NO_OP = 0;
-    public static final int ADD_OP = 1;
-    public static final int SUB_OP = 2;
-    public static final int MUL_OP = 3;
-    public static final int DIV_OP = 4;
+    String tmpzeroes = "";
+    int numZeroes = 0;
+    String tmpResults = "";
 
     Calculator calc = new Calculator();
 
@@ -36,98 +32,117 @@ public class MainActivity extends AppCompatActivity {
 
         tvHead = findViewById(R.id.tvHeader);
         tvDisp = findViewById(R.id.tvDisplay);
-
-        calc.setEntry(0.0);
-        calc.setResult(0.0);
-        calc.setDecPos(0);
-        calc.setCurrOp(NO_OP);
-
-   }
+    }
 
     public void btnOnClick(View view) {
 
-//        tmp = tvDisp.getText().toString();
-        currOp = calc.getCurrOp();
-        tmpEntry = calc.getEntry();
-        tmpDecPos = calc.getDecPos();
-        tmpResult = calc.getResult();
+        Log.d(TAG, "btnOnClick:");
         btn = findViewById(view.getId());
         key = btn.getText().toString();
 
-        //Log.d("TAG", "btnOnClick: R.id.btn1: " + R.id.btn1);
-        Log.d("TAG", "btnOnClick: key: " + key);
+        // send button to calculator
+        calc.btnClick(key);
 
-        switch (key){
-
-            case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0":
-                if (tmpDecPos > 0) {
-                    calc.setEntry((tmpEntry) + (Double.parseDouble(key) * Math.pow(10, -tmpDecPos)));
-                    tmpDecPos++;
-                    calc.setDecPos(tmpDecPos);
-                } else {
-                    calc.setEntry((tmpEntry * 10.0) + Double.parseDouble(key));
-                }
-                tvDisp.setText(calc.getEntry() + "");
-                break;
-            case ".":
-                if (tmpDecPos == 0) calc.setDecPos(calc.getDecPos() + 1);
-                tvDisp.setText(calc.getEntry() + "");
-                break;
-            case "+":
-                tvHead.setText(key);
-                if (currOp == 0){ // After Start Up OR  After "="
-                    if (tmpEntry > 0) { // Entry in progress
-                        if (tmpResult == 0){ // No Result yet
-                            calc.setResult(tmpEntry);
-                            calc.setEntry(0.0);
-                            calc.setCurrOp(ADD_OP);
-                            tvDisp.setText(calc.getResult() + "");
-                        } else { // Existing Result
-                            calc.setResult(tmpEntry);
-                            calc.setEntry(0.0);
-                            calc.setCurrOp(ADD_OP);
-                            tvDisp.setText(calc.getResult() + "");
-                        }
-                    } else { // No Entry yet)
-                        if (tmpResult > 0) { // Adding to existing Result
-                            calc.setCurrOp(ADD_OP);
-                        } // else since No Result AND No Entry, Do Nothing
-                    }
-                } else if (currOp > 0) { // Operation Key already Hit
-                    if (tmpEntry > 0) { // Entry in progress
-                        if (tmpResult == 0){ // No Result yet
-                            calc.setResult(tmpEntry);
-                            calc.setEntry(0.0);
-                            calc.setCurrOp(ADD_OP);
-                            tvDisp.setText(calc.getResult() + "");
-                        } else { // Existing Result
-                            calc.execute();
-                            calc.setEntry(0.0);
-                            calc.setCurrOp(ADD_OP);
-                            tvDisp.setText(calc.getResult() + "");
-                        }
-                    } else { // No Entry yet)
-                        if (tmpResult > 0) { // Adding to existing Result
-                            calc.setCurrOp(ADD_OP);
-                        } // else since No Result AND No Entry, Do Nothing
-                    }
-                }
-                break;
-            case "-":
-                calc.setCurrOp(SUB_OP);
-                calc.setResult(tmpResult - tmpEntry);
-                break;
-            case "x":
-                calc.setCurrOp(MUL_OP);
-                calc.setResult(tmpResult * tmpEntry);
-                break;
-            case "/":
-                calc.setCurrOp(DIV_OP);
-                calc.setResult(tmpResult / tmpEntry);
-                break;
-            case "=":
-                calc.setCurrOp(NO_OP);
-
+        // refresh display with calculator states
+        numZeroes = calc.getZeroes();
+        tmpzeroes = "";        for (int i = 0; i < numZeroes; i++){
+            tmpzeroes = tmpzeroes + "0";
         }
+        tvDisp.setText(calc.getEntry() + tmpzeroes);
+
+
+        switch (key) {
+            case "+":
+            case "-":
+            case "x":
+            case "/":
+                tmpResults = key;
+                break;
+            default:
+                tmpResults = "";
+        }
+        tvHead.setText(calc.getResult() + tmpResults);
+
+
+//        tmp = tvDisp.getText().toString();
+//        currOp = calc.getCurrOp();
+//        tmpEntry = calc.getEntry();
+//        tmpDecPos = calc.getDecPos();
+//        tmpResult = calc.getResult();
+//
+//        //Log.d("TAG", "btnOnClick: R.id.btn1: " + R.id.btn1);
+//
+//        switch (key){
+//
+//            case "1": case "2": case "3": case "4": case "5": case "6": case "7": case "8": case "9": case "0":
+//                if (tmpDecPos > 0) {
+//                    calc.setEntry((tmpEntry) + (Double.parseDouble(key) * Math.pow(10, -tmpDecPos)));
+//                    tmpDecPos++;
+//                    calc.setDecPos(tmpDecPos);
+//                } else {
+//                    calc.setEntry((tmpEntry * 10.0) + Double.parseDouble(key));
+//                }
+//                tvDisp.setText(calc.getEntry() + "");
+//                break;
+//            case ".":
+//                if (tmpDecPos == 0) calc.setDecPos(calc.getDecPos() + 1);
+//                tvDisp.setText(calc.getEntry() + "");
+//                break;
+//            case "+":
+//                tvHead.setText(key);
+//                if (currOp == 0){ // After Start Up OR  After "="
+//                    if (tmpEntry > 0) { // Entry in progress
+//                        if (tmpResult == 0){ // No Result yet
+//                            calc.setResult(tmpEntry);
+//                            calc.setEntry(0.0);
+//                            calc.setCurrOp(ADD_OP);
+//                            tvDisp.setText(calc.getResult() + "");
+//                        } else { // Existing Result
+//                            calc.setResult(tmpEntry);
+//                            calc.setEntry(0.0);
+//                            calc.setCurrOp(ADD_OP);
+//                            tvDisp.setText(calc.getResult() + "");
+//                        }
+//                    } else { // No Entry yet)
+//                        if (tmpResult > 0) { // Adding to existing Result
+//                            calc.setCurrOp(ADD_OP);
+//                        } // else since No Result AND No Entry, Do Nothing
+//                    }
+//                } else if (currOp > 0) { // Operation Key already Hit
+//                    if (tmpEntry > 0) { // Entry in progress
+//                        if (tmpResult == 0){ // No Result yet
+//                            calc.setResult(tmpEntry);
+//                            calc.setEntry(0.0);
+//                            calc.setCurrOp(ADD_OP);
+//                            tvDisp.setText(calc.getResult() + "");
+//                        } else { // Existing Result
+//                            calc.execute();
+//                            calc.setEntry(0.0);
+//                            calc.setCurrOp(ADD_OP);
+//                            tvDisp.setText(calc.getResult() + "");
+//                        }
+//                    } else { // No Entry yet)
+//                        if (tmpResult > 0) { // Adding to existing Result
+//                            calc.setCurrOp(ADD_OP);
+//                        } // else since No Result AND No Entry, Do Nothing
+//                    }
+//                }
+//                break;
+//            case "-":
+//                calc.setCurrOp(SUB_OP);
+//                calc.setResult(tmpResult - tmpEntry);
+//                break;
+//            case "x":
+//                calc.setCurrOp(MUL_OP);
+//                calc.setResult(tmpResult * tmpEntry);
+//                break;
+//            case "/":
+//                calc.setCurrOp(DIV_OP);
+//                calc.setResult(tmpResult / tmpEntry);
+//                break;
+//            case "=":
+//                calc.setCurrOp(NO_OP);
+//
+//        }
     }
 }
